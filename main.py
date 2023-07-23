65d21dfd8b0040b84fb7a84924e90da30550b12d
CarolinaH <carolinaheredialozano@gmail.com>
2023-07-23 16:40:38 -0500

    Started by creating the main file with a main menu and an option selection.

---
+++ main.py
@@ -0,0 +1,29 @@
+
+def importBalance():
+    balance = 0
+
+    return balance
+
+def menu():
+
+    print("____________MENU_____________")
+    print("1. Balance")
+    print("2. Current Goal status")
+    print("3. New Goal")
+    print("_____________________________")
+    print()
+
+    option = int(input())
+
+    return option
+
+def main():
+
+    print("_____________________________")
+    print("PERSONAL FINANCIAL TRACKER")
+    print("_____________________________")
+    print()
+
+    menu()
+
+main()
