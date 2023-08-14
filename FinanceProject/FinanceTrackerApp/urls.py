ede544bc6d21f871b8c4aa312868ce81762142e5
CarolinaH <carolinaheredialozano@gmail.com>
2023-08-13 20:25:19 -0500

    Added a signin and singup page. Working on getting the static settings set up appropriately.

--- FinanceProject/FinanceTrackerApp/urls.py
+++ FinanceProject/FinanceTrackerApp/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
+    path('goal', views.goal, name='goal'),
+    path('about', views.settings, name='settings'),
+    
 ]
--- FinanceProject/FinanceTrackerApp/views.py
+++ FinanceProject/FinanceTrackerApp/views.py
@@ -3,7 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    about = {'about': "H Financial is a Colombia based web application designed to help you find the simplest way to manage your personal finances."}
+    return render(request, "index.html", about)
+
+def signup(request):
+    return render(request, "signup.html")
+
+def signin(request):
+    return render(request, "signin.html")
+
+def signout(request):
+    pass
 
 def goal(request):
     return render(request, "goal.html")
