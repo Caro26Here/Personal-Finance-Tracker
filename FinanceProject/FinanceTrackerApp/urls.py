2350c8c1ff816a1362d6d7f0b5cdef3c0d3f8661
CarolinaH <carolinaheredialozano@gmail.com>
2023-08-15 16:21:52 -0500

    Created a user, and a landing page for the successful login.

--- FinanceProject/FinanceTrackerApp/urls.py
+++ FinanceProject/FinanceTrackerApp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
+    path('index', views.index, name='index'),
     path('signout', views.signout, name='signout'),
     path('goal', views.goal, name='goal'),
     path('settings', views.settings, name='settings'),
--- FinanceProject/FinanceTrackerApp/views.py
+++ FinanceProject/FinanceTrackerApp/views.py
@@ -2,19 +2,21 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def home(request):
     about = {'about': "H Financial is a Colombia based web application designed to help you find the simplest way to manage your personal finances."}
-    return render(request, "index.html", about)
+    
+    return render(request, "home.html", about)
 
 def signup(request):
 
     if request.method == "POST":
-        # username = request.POST.get('username') also works.
+
         username = request.POST.get('username')
         firstname = request.POST.get('firstname')
-        lastname = request.POST.get('lastname')
+        lastname = request.POST.get('lastname') 
         email = request.POST.get('email')
         password = request.POST.get('password')
         pass2 = request.POST.get('pass2')
@@ -31,13 +33,36 @@ def signup(request):
     return render(request, "signup.html")
 
 def signin(request):
+
+    if request.method == "POST":
+
+        username = request.POST('username')
+        password = request.POST('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return redirect(request,'index', {'fname': fname})
+        else:
+            messages.error(request, "Bad credentials!")
+            return redirect('home')
+
+    
     return render(request, "signin.html")
+
+def index(request):
+    userName = User.first_name
+   
+    return render(request, "index.html", userName)
 
 def signout(request):
     pass
 
 def goal(request):
     goal = { 'goal': "Buy a house"}
+
     return render(request, "goal.html", goal)
 
 def settings(request):
