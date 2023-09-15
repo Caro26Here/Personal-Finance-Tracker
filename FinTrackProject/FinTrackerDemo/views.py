1aa2c14ca9c343ae385391f8c06415c856fae689
CarolinaH <carolinaheredialozano@gmail.com>
2023-09-15 10:53:43 -0500

    Followed the login tutorial and successfully used the login to create a testuser.

--- FinTrackProject/FinTrackerDemo/views.py
+++ FinTrackProject/FinTrackerDemo/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 @api_view()
 def home(request):
     return Response('Hello World!', status=status.HTTP_200_OK)
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
